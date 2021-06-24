046b9e8577e555f2b7effbdeba9d9ce2a9f295f1
FmM7 <62862443+FmM7@users.noreply.github.com>
2021-06-24 15:54:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ still = list(range(1, people_num + 1))
 
 def two_dim_vec(num):
     if isinstance(num, int) or isinstance(num, np.int64):
-        return np.array([num % wid, num // wid])
-    elif isinstance(num, np.ndarray):
+        return [num % wid, num // wid]
+    elif isinstance(num, list):
         return num[0] + num[1] * wid
 
 #縦hei,横widの零行列
@@ -41,6 +41,7 @@ print(still)
 eva_loc = np.random.choice(np.array(np.where(cell.reshape(-1,) == 0))
           .reshape(-1,), people_num, replace = False)
 np.put(cell, eva_loc, still)
+cell = [list(i) for i in cell]
 print(cell)
 for i in range(1, people_num + 1):
     eva_dict[i].loc = two_dim_vec(eva_loc[i - 1])
@@ -49,14 +50,13 @@ print(eva_dict)
 
 #2点間距離
 def dist(loc1, loc2):
-    loc1 = loc1 if isinstance(loc1, np.ndarray) else two_dim_vec(loc1)
-    loc2 = loc2 if isinstance(loc2, np.ndarray) else two_dim_vec(loc2)
+    loc1 = loc1 if isinstance(loc1, list) else two_dim_vec(loc1)
+    loc2 = loc2 if isinstance(loc2, list) else two_dim_vec(loc2)
     return math.sqrt((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2)
 
 def eva_move(loc):
     """
     避難者クラスを移動させる関数
-
     Args:
         loc (int or np.int64 or np.ndarray): 避難者の位置
     """
@@ -68,13 +68,13 @@ def eva_move(loc):
     #移動先の候補
     loc_opt = []
     if loc[0] != 0:
-        loc_opt.append(loc - [1, 0])
+        loc_opt.append([loc[0] - 1, loc[1]])
     if loc[0] != wid - 1:
-        loc_opt.append(loc + [1, 0])
+        loc_opt.append([loc[0] + 1, loc[1]])
     if loc[1] != 0:
-        loc_opt.append(loc - [0, 1])
+        loc_opt.append([loc[0], loc[1] - 1])
     if loc[1] != hei - 1:
-        loc_opt.append(loc + [0, 1])
+        loc_opt.append([loc[0], loc[1] + 1])
 
     move_result = []
     for i in loc_opt:
@@ -90,15 +90,15 @@ c = 0
 while True:
     #Eva毎に行動
     c += 1
-    print(cell)
-    print(still)
+    print(list(cell))
+    #print(still)
     escaped = []
     for i in still:
         here_loc = eva_dict[i].loc
         move_chance = eva_move(here_loc)
         result = rd.choices([i[0] for i in move_chance], [i[1] for i in move_chance])[0]
-        result_cell = cell[result[1], result[0]]
-        print(i,result,result_cell)
+        result_cell = cell[result[1]][result[0]]
+        #print(i,result,result_cell)
         if result_cell == 0:
             eva_dict[i].loc = result
             cell = np.where(cell == i, 0, cell)
@@ -116,6 +116,3 @@ while True:
         print("osoi")
         print(cell)
         exit()
-
-
-input()
