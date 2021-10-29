dea908d3e868d8c4108aafec30c7c3b84c2588be
FmM7 <62862443+FmM7@users.noreply.github.com>
2021-10-29 12:41:07 +0900

    Add files via upload

--- evacuee/index.js
+++ evacuee/index.js
@@ -4,6 +4,10 @@ class Evacuee {
         this.number = number;
         // ??は左オペランドがnullishなら右オペランドを採用
         this.position = position ?? [0, 0];
+        this.evacueeType = "evacuee";
+        this.move = () => {
+            const movingProbability = this.evacueeType === "evacuee" ? 1.0 : 0.5;
+        };
     }
 }
 
@@ -11,6 +15,7 @@ class Injured extends Evacuee {
     constructor(...args) {
         //superはthisを触る前
         super(...args);
+        this.evacueeType = "injured";
     }
 }
 
@@ -26,41 +31,51 @@ const [wid, hei] = [49, 49];
 const ks = 3.0;
 const peopleNumber = 144;
 const injuredPossibility = 0.05;
-const wholeCell = {}
-const stillEvacuation = []
+const wholeCell = {};
+const stillEvacuation = [];
 for (let i=1; i <= wid * hei; i++) {
     wholeCell[i] = new Cell();
 }
 wholeCell[Math.ceil(wid / 2)].state = "exit";
-for (let i=1; i <= peopleNumber; i++) {
-    stillEvacuation.push(i);
-}
 
 const HTMLUpdateCell = function() {
     const HTMLCellTable = document.createElement("table");
     HTMLCellTable.setAttribute("id", "cell-table");
-    const HTMLCellTr = document.createElement("tr");
-    for (let i=1; i<=wid; i++) {
-        HTMLCellTr.appendChild(document.createElement("td"));
-    }
     for (let i=1; i<=hei; i++) {
+        const HTMLCellTr = document.createElement("tr");
+        for (let j=1; j<=wid; j++) {
+            const HTMLCellTd = document.createElement("td");
+            if (wholeCell[(i-1) * wid + j].state !== "empty") {
+                HTMLCellTd.setAttribute("class", wholeCell[(i-1) * wid + j].state);
+            }
+            HTMLCellTr.appendChild(HTMLCellTd.cloneNode(true));
+        }
         HTMLCellTable.appendChild(HTMLCellTr.cloneNode(true));
     }
     document.getElementById("eva-table")
         .replaceChild(HTMLCellTable, document.getElementById("cell-table"));
+    
+    
 }
 
-const evaDictionary = {}
+const evaDictionary = {};
 for (let i=1; i <= peopleNumber; i++) {
     const isInjured = Math.random() < injuredPossibility;
     evaDictionary[i] =
-        isInjured ? new Injured(i, 0) : new Evacuee(i, 0);
-    let evacueePosition = 0;
+        isInjured
+        ? new Injured(i, 0)
+        : new Evacuee(i, 0);
+    stillEvacuation.push(i);
+    let evacueePosition;
     do {
         evacueePosition = Math.floor(Math.random() * (wid * hei) + 1);
         evaDictionary[i].position =
             [evacueePosition % wid, Math.ceil(evacueePosition / wid)];
     } while (wholeCell[evacueePosition].state !== "empty");
-    wholeCell[evacueePosition].state = "evacuee";
+    wholeCell[evacueePosition].state = evaDictionary[i].evacueeType;
 }
 
+
+window.onload = (event) => {
+    HTMLUpdateCell();
+};
